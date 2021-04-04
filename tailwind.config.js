da5f7795e64b9978aa799f8c09a5b1e936a5b313
Alvin Ashiatey <alvinashiatey@gmail.com>
2021-04-03 22:01:04 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	purge: [
+        './htdocs/site/templates/*.php',
+        './htdocs/site/templates/**/*.php',
+        './htdocs/site/snippets/*.php',
+        './htdocs/site/snippets/**/*.php',
+	],
+	theme: {
+		extend: {
+		}
+	}
+}
